2c71f46caf8e3977ed86e4565a12b27101340534
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-08-27 22:46:00 +0400

    Add responsive navbar with hamburger menu
    Integrated 'hamburger-react' for mobile navigation and updated BlogPage layout to support responsive design. Updated CSS for global font and navbar styles. Added Hamburger component and refactored BlogPage to show/hide links based on screen size.

--- src/BlogPage.jsx
+++ src/BlogPage.jsx
@@ -1,31 +1,43 @@
 import { Link } from "react-scroll";
+import { useTheme, useMediaQuery } from "@mui/material";
 import { Box } from "@mui/material";
 import logo from "./assets/logo.png";
 import "./BlogPage.css";
-import { BsArrowDownRight } from "react-icons/bs";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
 
 const BlogPage = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
+  const list_links = (
+    <Box>
+      <Link className="link">About me</Link>
+      <Link className="link">Experience</Link>
+      <Link className="link">Education</Link>
+      <Link className="link">Projects</Link>
+      <Link className="link">Publications</Link>
+      <Link className="link">Recommendations</Link>
+    </Box>
+  );
+  const [isOPen, setIsOpen] = useState(false);
+
   return (
     <Box>
-      <Box className="all_links">
+      <Box className="all_navbar">
         <img src={logo} width="100" height="100" />
-        <Link className="link" to="home" smooth={true} duration={500}>
-          About me
-        </Link>
-        <Link className="link">Experience</Link>
-        <Link className="link ">Education</Link>
-        <Link className="link">Projects</Link>
-        <Link className="link">Publications</Link>
-        <Link className="link">Recommendations</Link>
-        <Box className="hire_me_button">
-          <span>Hire me</span>
+        {!isMobile ? list_links : <></>}
+        <Box className="hire_me_and_hamburger">
+          <Box className="hire_me_button">
+            <span>Hire me</span>
+          </Box>
+          {isMobile ? <Hamburger toggled={isOPen} toggle={setIsOpen} /> : <></>}
         </Box>
       </Box>
-      <Box>
-        <section id="home">
-          <h1>Section About me</h1>
-        </section>
-      </Box>
+      {isOPen && isMobile ? (
+        <Box className="when_burger_isOpen">{list_links} </Box>
+      ) : (
+        <></>
+      )}
     </Box>
   );
 };
---
+++ src/container/Hamburger.jsx
@@ -0,0 +1,14 @@
+/* import Hamburger from "hamburger-react";
+import { useState } from "react";
+
+const HamburgerComponent = () => {
+  const [open, SetOpen] = useState(false);
+
+  return (
+    <div>
+      <Hamburger toggle={SetOpen} toggled={open} />
+    </div>
+  );
+};
+export default HamburgerComponent;
+ */
