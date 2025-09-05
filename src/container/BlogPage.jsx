1fbd872e9d7b6962af760452885d76389c473243
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-09-05 13:00:59 +0400

    add text line

--- src/container/BlogPage.jsx
+++ src/container/BlogPage.jsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import Navbar from "./navbar";
 import "./BlogPage.css";
 import Introduction_section from "./introduction_section";
+import Line_Text from "./Text_Line";
 
 const BlogPage = () => {
   return (
@@ -9,6 +10,7 @@ const BlogPage = () => {
       <Box>
         <Navbar />
         <Introduction_section />
+        <Line_Text />
       </Box>
     </Box>
   );
---
+++ src/container/Text_Line.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+const Line_Text = () => {
+  return (
+    <Box className="flex justify-between bg-red text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+      <p>FAST LEANER</p>
+      <p>RESULT- DRIVEN</p>
+      <p>GROWTH- MINDED</p>
+      <p>ANALYTICAL THINKER</p>
+    </Box>
+  );
+};
+
+export default Line_Text;
