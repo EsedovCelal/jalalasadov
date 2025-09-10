be7c00d07482253ab6f4d0edc3930fb6517eede8
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-09-10 16:30:23 +0400

    bug fix

--- src/container/navbar.jsx
+++ src/container/navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const dispatch = useDispatch();
   const isOpen = useSelector((state) => state.menu.isOpen);
   const styles = {
-    link: "after:content-[''] after:absolute after:h-[2px] after:left-[0] after:bottom-[0] after:w-[0] after:bg-[red] after:[transition:width_0.9s] hover:text-[#EEEEEE] hover:[transition:color_0.9s_ease] text-[1.25rem] [transition:color_0.9s_ease] cursor-pointer relative ml-[20px] after:absolute after:h-[2px] after:left-[0] after:bottom-[0] after:w-[0] after:bg-[red] after:[transition:width_0.9s] hover:after:w-full",
+    link: "text-[#EEEEEE] after:content-[''] after:absolute after:h-[2px] after:left-[0] after:bottom-[0] after:w-[0] after:bg-[red] after:[transition:width_0.9s] hover:text-[#00ADB5] hover:[transition:color_0.9s_ease] text-[1.25rem] [transition:color_0.9s_ease] cursor-pointer relative ml-[20px] after:absolute after:h-[2px] after:left-[0] after:bottom-[0] after:w-[0] after:bg-[#00ADB5] after:[transition:width_0.9s] hover:after:w-full",
   };
   const list_links = (
     <div className="max-[1280px]:hidden">
@@ -23,14 +23,14 @@ const Navbar = () => {
   );
   return (
     <div className="flex justify-center">
-      <div className="w-[70%] flex justify-between items-center bg-[#00ADB5] px-[40px] py-[0] border-[1px] border-[#00ADB5] rounded-[10px] mt-[5px]">
+      <div className="w-[70%] flex justify-between items-center px-[40px] py-[0] border-[1px] border-[#00ADB5] rounded-[10px] mt-[5px]">
         <img src={logo} className="w-[100px] h-[100px]" />
         {list_links}
         <div className="flex justify-between">
           <div className="hire_me_button">
             <span>Hire me</span>
           </div>
-          <div className="min-[1280px]:hidden">
+          <div className=" min-[1280px]:hidden">
             <Hamburger
               easing="ease-in"
               toggled={isOpen} // control the toggle state
