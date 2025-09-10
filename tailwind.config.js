d01a7c5e20d4a070ee65f22791c8220d47ac4948
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-09-10 15:35:47 +0400

    change tailwindcss and add background color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      keyframes: {
-        bgmove: {
-          "0%": { backgroundPosition: "0 0" },
-          "100%": { backgroundPosition: "0 100%" }, // match your bg-size for smooth repeat
-        },
-      },
-      animation: {
-        bgmove: "bgmove 40s infinite",
-      },
-    },
-  },
   plugins: [],
 };
