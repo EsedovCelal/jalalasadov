e49d7dc65dc9457dd376e05eb1ccb51355265548
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-10-16 16:22:44 +0400

    bug fix add recemmendations

--- src/container/Recommendations.jsx
+++ src/container/Recommendations.jsx
@@ -1,19 +1,12 @@
 import { Element } from "react-scroll";
-import SimpleImageSlider from "react-simple-image-slider";
-import jalalasadov from "../assets/img/temp2.jpg";
+import temp2 from "../assets/img/temp2.jpg";
 
 const Recommendations = () => {
-  const images = [
-    {
-      url: jalalasadov,
-    },
-    {
-      url: jalalasadov,
-    },
-  ];
   return (
-    <Element name="Recommendations" className="text-white">
-      <p className="text-6xl text-center mb-[15px]">Recommendations</p>
+    <Element name="Recommendations" className="text-white px-8 pt-15">
+      <p className="text-4xl md:text-5xl lg:text-6xl text-center mb-[15px]">
+        Recommendations
+      </p>
       <p className="text-lg text-center mb-[50px]">
         I’m grateful for the professional relationships I’ve built and the
         positive impact I’ve made along the way. Below are selected
@@ -23,14 +16,8 @@ const Recommendations = () => {
         effective teamwork.
       </p>
       <div className="w-full border-2 border-gray-300 hidden lg:block mb-[50px]"></div>
-      <div className="flex justify-center items-center">
-        <SimpleImageSlider
-          width="100%"
-          height="auto"
-          images={images}
-          showNavs={true}
-          loop={true}
-        />
+      <div className="flex justify-center items-center ">
+        <img src={temp2} className="w-full h-auto object-cover max-w-200" />
       </div>
       <div className="flex justify-center imtes-center">
         <a
